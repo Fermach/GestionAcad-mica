27784c0e722dad3cc232341960ece4c88dc44376
juangualberto <jgutierrez@iesvirgendelcarmen.com>
2017-11-23 20:31:37 +0100

    Revisado Readme

--- web/js/index.js
+++ web/js/index.js
@@ -1,36 +1,37 @@
 
 $(document).ready(
         function () {
-            $(".panel").hide();
-            $("#main").show();
-            
+
             $('.button-collapse').sideNav({
                 menuWidth: 300, // Default is 300
                 edge: 'left', // Choose the horizontal origin
                 closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
                 draggable: true // Choose whether you can drag to open on touch screens
-              }
+            }
             );
             $('.button-collapse').sideNav('show');
             // $(".button-collapse").sideNav();
             $('.collapsible').collapsible();
             
-            
-        });
-        
-        console.log($('[id*=menu_]'));
+            $('[id^="menu_"]').each(function () {
+                var $this = $(this);
+                var menu_id = $this.attr('id');
+                var panel_id = $this.attr('id').replace('menu_', 'panel_');
+                
+                $("#" + menu_id).click(function () {
+                    $(".panel").hide();
+                    $("#" + panel_id).show();
+                });
+                console.log("id_menu::" + menu_id + "  id_panel" + panel_id);
+            });
 
-$("[id*=menu_]").each(function () {
-    var $id_menu = $(this).attr('id');
-    var id_panel = $id_menu.attr('id').replace('menu_', 'panel_');
-    $("#"+id_panel).click( function(){
-        $(".panel").hide();
-        $("#"+id_panel).show();
-    });
-    console.log("id_menu::"+ $(this) + "  id_panel"+id_panel);
-});
-        
-        
-        
-        
-        
+            $(".panel").hide();
+            $("#panel_main").show();
+
+        });
+
+
+
+
+
+
