fd7b4867d8999f3dee02b43b4ccac38da2d6b6f6
Juan Gualberto <jgutierrez@iesvirgendelcarmen.com>
2017-12-20 14:34:10 +0100

    Servicio autenticación modificado

--- src/java/com/iesvdc/acceso/controller/filters/AuthenticationService.java
+++ src/java/com/iesvdc/acceso/controller/filters/AuthenticationService.java
@@ -36,7 +36,7 @@ public class AuthenticationService {
                         // call some UserService/LDAP here
                         Conexion conexion = new Conexion();
                         Connection con = conexion.getConexion();
-                        String sql = "SELECT * FROM Usuarios WHERE username=? AND password=?";
+                        String sql = "SELECT * FROM USUARIO WHERE USERNAME=? AND PASSWORD=?";
                         PreparedStatement pstm = con.prepareStatement(sql);
                         pstm.setString(1, username);
                         pstm.setString(2, password);
